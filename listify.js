c99c04af019358e0e0f4d92ac6b64ea6225a9f19
Apiros3 <lemon211k04@gmail.com>
2023-09-16 00:49:41 +0900

    new post!

--- listify.js
+++ listify.js
@@ -4,28 +4,4 @@ var update = [];
 var genre = [];
 
 function listify() {
-
-    lst.push("事実は小説より奇なり");
-    update.push("2023-09-15")
-    genre.push("日記:")
-
-    lst.push("opalってアプリ");
-    update.push("2023-09-14")
-    genre.push("Other:")
-
-    lst.push("main-page案");
-    update.push("2023-09-13")
-    genre.push("Other:")
-
-    lst.push("買いたいものリスト");
-    update.push("2023-09-13")
-    genre.push("Other:")
-
-    lst.push("About");
-    update.push("2023-09-13")
-    genre.push("Other:")
-
-    lst.push("ブログ作った話");
-    update.push("2023-08-19")
-    genre.push("Other:")
 }
