d179dc184e51f1e5ba0bf058ced53696f5072c08
Apiros3 <lemon211k04@gmail.com>
2023-09-16 11:31:27 +0900

    new post!

--- listify.js
+++ listify.js
@@ -5,6 +5,10 @@ var genre = [];
 
 function listify() {
 
+    lst.push("競プロ用ブログ環境");
+    update.push("2023-09-16")
+    genre.push("制作物:")
+
     lst.push("高速素数判定");
     update.push("2023-09-16")
     genre.push("ライブラリ:")
