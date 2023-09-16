1c4766ca643636577b5f26d2edf37186bd03ec1f
Apiros3 <lemon211k04@gmail.com>
2023-09-16 10:23:18 +0900

    new post!

--- blogify.py
+++ blogify.py
@@ -27,6 +27,8 @@ function listify() {
             print(str(filename))
             text = f.read()
             html = markdown.markdown(text)
+            html = "".join([["\)","\("][i&1 if i else 1]+l for i, l in enumerate(html.split('$'))])[2:]
+            html = html.replace("<code>","<pre><code>").replace("</code>","</code></pre>")
             # print(str(html))
         genre = "Other:"
         filepath = str(filename[8:-3])
@@ -38,14 +40,23 @@ function listify() {
 
         with open(f"blog-html/{filepath}.html", 'w') as f:
             f.writelines("""
-<link rel="stylesheet" href="style.css">
-<script src="script.js"></script>
-                                                  
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous" onload="renderMathInElement(document.body);"></script>
+    <link rel="stylesheet" href="highlight/styles/github-dark-dimmed.min.css">
+    <script src="highlight/highlight.min.js"></script>
+</head>                           
 """)
             f.write(html)
             f.writelines("""
 <script>
-    init()
+    init();
+    hljs.highlightAll();
 </script>
 """)
         if (str(filename)[8:-3] == "default"):
